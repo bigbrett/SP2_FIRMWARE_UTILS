06a9c3bce698362f15ca3dd7d34e4a663f7552d3
Brett Nicholas - MacbookPro <Brett.R.Nicholas.15@Dartmouth.edu>
2015-09-09 14:35:11 -0600

    finished with first iteration

--- scripts/language_support/update_translations.pl
+++ scripts/language_support/update_translations.pl
@@ -1,21 +1,23 @@
 #!/usr/bin/perl -s
 #==============================================================================================
 #
-#         FILE:  update_languages.pl
+#         FILE:  update_translations.pl 
 #
-#        USAGE:  ./update_languages.pl <CSV_FILE.csv> <HEADER_FILE.h>
+#        USAGE:  perl update_translations.pl <CSV_FILE.csv> <HEADER_FILE.h>
 #
 #  DESCRIPTION:  This script parses a CSV file containing translated
-#                strings, and updates the strings in the C data structure
-#                to match the CSV file
+#                strings and their variable names, and updates the strings 
+#                in the C data structure to match the CSV file. 
+#                Ensure the CSV language table is up-to-date, and then call this script 
+#                as detailed above. 
 #
 #      OPTIONS:  ---
 #
 #    ARGUMENTS:  1) <CSV_FILE.csv> - CSV file containing translated strings of the format below
 #
-#                       english,french,german,italian,spanish
-#                       hello,bonjour,hallo,ciao,hola
-#                       thanks,merci,danke,grazie,gracias
+#                       str_VAR_NAME,english,french,german,italian,spanish
+#                       str_HELLO,hello,bonjour,hallo,ciao,hola
+#                       str_THANK_YOU,thank you,merci,danke,grazie,gracias
 #                       ...
 #
 #                2) <HEADER_FILE.h> - C header file containing language string data structure
@@ -30,160 +32,148 @@
 #
 #                   
 #                       /* String data structure declaration */
-#                       typedef struct {
+#                       typedef struct 
+#                       {
 #                           uint16_t const string_lens[NUM_LANGUAGES]; // Maximum string length 
 #                           const char* language_ptrs[NUM_LANGUAGES]; // Array of string pointers
 #                       } sp2_string_struct_t;
 #
 #
 #                       /* String data structure instantiation */ 
-#                       static const sp2_string_struct_t string_array[NUM_STRINGS] = {
-#                           /* str_HELLO */ 
-#                           { {5,7,5,4,4},
-#                           {   "HELLO",     // ENGLISH
-#                               "BONJOUR",   // FRENCH
-#                               "HALLO",     // GERMAN
-#                               "CIAO",      // ITALIAN 
-#                               "HOLA"       // SPANISH 
-#                           } },
-#
-#                           /* str_THANKS */ 
-#                           { {6,5,5,6,7},
-#                           {   "THANKS",     // ENGLISH
-#                               "MERCI",      // FRENCH
-#                               "DANKE",      // GERMAN
-#                               "GRAZIE",     // ITALIAN 
-#                               "GRACIAS"     // SPANISH 
-#                           } },
-#                           
+#                       static const sp2_string_struct_t string_array[NUM_STRINGS] = 
+#                       {
+#                           /* str_HELLO */ {{5,7,5,4,4},{"HELLO","BONJOUR","HALLO","CIAO","HOLA"}},
+#                           /* str_THANK_YOU */ { {6,5,5,6,7},{"THANKS","MERCI","DANKE","GRAZIE","GRACIAS"}},
 #                           // ... lots of these
 #                       }; 
 #
 # REQUIREMENTS:  perl version 5.014 or newer 
 #         BUGS:  ---
-#        NOTES:  Copy of old header file stored in <old_header>.h.orig
+#        NOTES:  Copy of old header file is stored in <old_header>.h.bak
 #       AUTHOR:  Brett Nicholas
 #      COMPANY:  Avatech
 #      VERSION:  1.0
 #      CREATED:  08/10/2015
-#     REVISION:  1
+#     REVISION:  2
 #==============================================================================================
 use 5.014;
 use strict;
 use warnings;
 no warnings 'uninitialized';
+use Data::Dumper;
+use Tie::File;
+use File::Copy; 
+
+# Get file names from command line
+my $csvfile = "copy.csv";      
+my $headerfile = "sp2_string.h";
+my $backupfile= "$headerfile.bak"; 
+
+# Global variables
+my %langHash; # hash table holding language arrays, indexed by elements of @langs
+my @langs; # array holding languages (hash keys to %langHash) 
+my @str_names; # array holding string labels (hash keys to %{ $langHash{$lang} })
+
+# Create backup of original header file with .orig extension
+copy($headerfile,$backupfile) or die "Failed to create backup file: $!\n"; 
 
 
-our ($convert); # command line option to strip DOS line endings using dos2unix program 
-
-
-(my $csvfile, my $headerfile) = @ARGV;      # get file names from command line
-my $tempfile = "tempfile";                  # name of temporary file
-
-
-my @langs = ("english","french","german","italian","spanish"); # array holding what languages are used
-my (%french, %german, %italian, %spanish);  # hash table for each language where hash key is english string
-my @english_keys; # array holding all english strings (keys)
-my $key;
-
-
-print "OS = $^O\n\n"; # Print operating system 
-# If running linux or mac, get rid of DOS-style line endings with BASH system call to dos2unix program
-if ($convert) { 
-	system('command -v dos2unix >/dev/null 2>&1 || { echo >&2 "I require foo but it\'s not installed.  Aborting."; exit 1; }') == 0 
-			or die "FATAL ERROR: missing program: dos2unix\n\tdos2unix can be downloaded at http://sourceforge.net/projects/dos2unix or can be installed via your OS's native package manager (apt-get, cyg-apt, homebrew, etc. ). "; 
-	system("dos2unix $headerfile $csvfile");    # get rid of DOS-style line endings with BASH system call to dos2unix program
-}
-else {
-    warn "WARNING: File may contain MS-DOS line endings, which could cause unwanted CRLF metacharacters to be inserted into strings (or other formatting issues).\nTo ensure line endings are *NIX compatible, use command-line option [ -convert ] to strip DOS CRLF characters using dos2unix program'\n";
-}
-
-
-open (my $fh_csv,'<', $csvfile) or die "Couldn't read '$csvfile': $!";      # open csv file for reading
+# --------------------------------------------------------------------------------
+# ------------------ Parse CSV file to get updated strings -----------------------
+# --------------------------------------------------------------------------------
+print "\n...Parsing CSV file for updated values...\n";
+open (my $fh_csv,'<', $csvfile) or die "Couldn't read '$csvfile': $!"; # open csv file for reading
 while ( <$fh_csv> ) # parse file line by line
 {
     # split $_ by commas
     my @fields = split /,/;
 
-    # store each comma-separated string in correct language hash
-    if (($. != 1) and ($#fields+1 == 5))
+    # Each field of 1st line is stored in @langs
+    if ($. == 1) 
     {
-        push(@english_keys, $fields[0]);        # english array
-        $french{ $fields[0] } = $fields[1];     # french is second column
-        $german{ $fields[0] } = $fields[2];     # german is third column
-        $italian{ $fields[0] } = $fields[3];    # italian is fourth column
-
-        chomp $fields[4];                       #remove trailing newline from last field
-        $spanish{ $fields[0] } = $fields[4];    #spanish is fifth column
+        @langs=@fields[1 .. $#fields]; # ignore first column 
+        my $numlangs=$#langs; # number of languages declared
+    }
+    elsif (($. != 1) and ($#fields+1 == 6))  # if not first line and store each comma-separated string in correct language hash
+    {
+        push(@str_names, $fields[0]); # push string variable names onto array
+        my @curr_strs = @fields[1 .. $#fields]; # store language strings in current string array
+        
+        # store each string in appropriate language hash array 
+        my $i=0; 
+        foreach my $string (@curr_strs) 
+        {
+            chomp $string; # remove possible trailing newline 
+            ${ $langHash{$langs[$i++]} }{ $fields[0] } = $string; 
+        }
     }
 }
 close $fh_csv or die "couldn't close '$csvfile' : $!"; #close file
 
 
+# --------------------------------------------------------------------------------
+# ------------ Rewrite string variable name declaration in header file ----------- 
+# --------------------------------------------------------------------------------
+print "\n...Updating string variable names...\n"; 
 open (my $fh_header, '<', $headerfile) or die "Couldn't read '$headerfile': $!"; # open header file for reading
-open (my $fh_tempfile, '>', $tempfile) or die "Couldn't create '$tempfile': $!"; # open temporary file for writing
-
-
-# print header 
-print "\nReplacing strings in $headerfile. Changes listed below...\n";
-print "\nstr_STRING_NAME   (language):   \"old_string\" --> \"new_string\"\n";
-print " --------------------------------------------------------------\n"; 
-
-
-my $string_count = 0; # counts number of english strings processed 
-my $error_line = 0; # holds line of first error
-my $change_flag = 0; # flag incremented if any string is changed. 0 = no changes
-my $curr_str_name; # holds name of current string
-
-
+my ($startlinenum, $endlinenum, $numstringlines) = 0; # start and end line number for screen replacement 
 while ( <$fh_header> )  # parse read-only file line by line
 {
-    # if line falls within language struct declaration
-    if ( /^static const sp2_string_struct_t string_array\[NUM_STRINGS\] =/.../end string_array/ )
+    # get starting line of string structure declaration
+    /typedef enum \/\*SP2_STRING_T__PERL_START\*\// and $startlinenum=$.+1; 
+
+    # count number of lines to be replaced 
+    /typedef enum \/\*SP2_STRING_T__PERL_START\*\// .. /NUM_STRINGS/ and $numstringlines++ and $endlinenum=$.; 
+}
+close $fh_header, $headerfile or die "Couldn't close '$headerfile': $!";
+
+my @sub_strs = map{"    $_" . ","} @str_names; # format variable names
+
+# replace string variable names in header with those from CSV 
+tie my @flines, 'Tie::File', $headerfile or die "error using Tie to read file <$headerfile>";  # tie file lines to array 
+splice(@flines,$startlinenum,($endlinenum-$startlinenum-1), @sub_strs); 
+untie @flines;  
+
+
+# --------------------------------------------------------------------------------
+# ------- rewrite all fields in string structure in header file -------
+# --------------------------------------------------------------------------------
+print "\n...Updating string structure contents...\n";
+open ($fh_header, '<', $headerfile) or die "Couldn't read '$headerfile': $!"; # open header file for reading
+($startlinenum, $endlinenum, $numstringlines) = 0; # start and end line number for screen replacement 
+while (<$fh_header>) # loop through file to get start and end line number for replacement
+{
+    /static const sp2_string_struct_t string_array\[NUM_STRINGS\]/ and $startlinenum=$.+1; 
+    /static const sp2_string_struct_t string_array\[NUM_STRINGS\]/ .. /end string_array\[\]/ and $numstringlines++ and $endlinenum=$.; 
+}
+close $fh_header, $headerfile or die "Couldn't close '$headerfile': $!";
+
+
+my @newlines; # new array to be spliced into @flines
+my (@templen, @tempstr); # dummy array containers to allow push/pop of strings
+my ($commentlabel,$lens,$strs,$currline); # concatenate substrings to form $currline which is then pushed onto @newlines
+
+# loop over hash structure and construct each line of the declaration, then push onto @newlines
+foreach my $str_name (@str_names)
+{
+    $commentlabel = "/*" . $str_name . "*/"; # start each line with a commented string name 
+    foreach my $lang (@langs)
     {
-				# get current string label	
-        /\/\*.*(str_.*?)\*\//s and $curr_str_name=$1;
-
-        # get english translation hash key
-        /$langs[0]/i and /"(.*?)"/i and $key="$1" # if line contains "english", $key=string between quotes
-        and ( ($key eq $english_keys[$string_count]) # make sure english string in header matches csv string or DIE 
-                or ( ($error_line = $string_count+3) and die "\nERROR: ENGLISH STRING MISMATCH \n | \($headerfile - line $.\): \"$key\" \n | \($csvfile - line $error_line\): \"$english_keys[$string_count]\"\n >>> program terminated at" ) )
-	    and $string_count++;
-
-        # Check that translations exist
-        /$langs[1]/i and !$french{$key} and warn "WARNING: no $langs[1] translation for $key";
-        /$langs[2]/i and !$german{$key} and warn "WARNING: no $langs[2] translation for $key";
-        /$langs[3]/i and !$italian{$key} and warn "WARNING: no $langs[3] translation for $key";
-        /$langs[4]/i and !($spanish{$key}) and warn "WARNING: no $langs[4] translation for $key";
-
-        # replace each language string with new hash value  by replacing string between
-        # quotation marks with $<language>{$key} on lines that contain "<language>"
-        /$langs[1]/i and s/"(.*?)"/"$french{$key}"/g
-					and (($1 eq $french{$key}) or ( print "  $curr_str_name \($langs[1]\): \"$1\" --> \"$french{$key}\"\n" and $change_flag++ ));
-        /$langs[2]/i and s/"(.*?)"/"$german{$key}"/g
-					and (($1 eq $german{$key}) or ( print "  $curr_str_name \($langs[2]\): \"$1\" --> \"$german{$key}\"\n" and $change_flag++ ));
-        /$langs[3]/i and s/"(.*?)"/"$italian{$key}"/g
-					and (($1 eq $italian{$key}) or ( print "  $curr_str_name \($langs[3]\): \"$1\" --> \"$italian{$key}\"\n" and $change_flag++ ));
-        /$langs[4]/i and s/"(.*?)"/"$spanish{$key}"/g 
-					and (($1 eq $spanish{$key}) or ( print "  $curr_str_name \($langs[4]\): \"$1\" --> \"$spanish{$key}\"\n" and $change_flag++ ));
+        push (@tempstr, "\"" . ${$langHash{$lang}}{$str_name} . "\""); #translated strings
+        push (@templen, length ${$langHash{$lang}}{$str_name}) # push lengths
     }
-    print $fh_tempfile "$_" or die "ERROR: Couldn't write to $tempfile: $!"; # print output to tempfile
+    $lens = "{" . join(",",@templen) . "}" and @templen= (); # create syntactically correct length array field
+    $strs = "{" . join(",",@tempstr) . "}" and @tempstr= (); # create syntactically correct string array field
+    $currline = $commentlabel . " \{" . $lens . "," . $strs . "\}" and push(@newlines,$currline); # format current line and push current line onto new line
 }
 
+@newlines = join (",\n",@newlines); # separate lines with comma and newline
 
-# close files
-close $fh_header, $headerfile or die "Couldn't close '$headerfile': $!";
-close $fh_tempfile, $tempfile or die "Couldn't close '$tempfile': $!";
-
-
-# rename tempfile to sp2_string.h and rename original to sp2_string.h.orig
-rename $headerfile, "$headerfile.orig";
-rename $tempfile, $headerfile;
+tie @flines, 'Tie::File', $headerfile or die "error using Tie to read file <$headerfile>"; # tie file lines to array 
+splice(@flines,$startlinenum,($endlinenum-$startlinenum-1), @newlines); # replace line range with @newlines
+untie @flines;
 
 
-# print success message 
-($change_flag == 0) and print "  ... * no strings updated *\n"; # no strings updated
-print "\nLanguage strings successfully updated! Old header saved as  \'$headerfile.orig\' \n";
 
+END { print "\n\n$headerfile updated successfully!\n\n";} 
 
-END { print "\n";} 
